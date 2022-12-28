d8c524e265792f20abbaf7ebb17936b0db846adb
Santiago Villaverde <zantyvillaverde@hotmail.com>
2022-12-28 14:27:13 -0300

    primera pre entrega fix

--- JS/main.js
+++ JS/main.js
@@ -21,15 +21,15 @@ if(currency == "dolares"){
     alert("El porcentaje de interes anual en dolares es del 6%")
     let mount = parseInt(prompt("Escribe la cantidad de dinero que vas dejar en el plazo fijo: "))
     let days = parseInt(prompt("Escribe la cantidad de dias para calcular el interes: "))
-    let restult = (mount * 0.06 / days);
+    let restult = (mount * 0.06 / days).toFixed(2);
     alert("El interes generado por dejar $" + mount + " en el plazo fijo durante " + days + "Es " + restult);
 
 }else if(currency == "pesos"){
     alert("El porcentaje de interes anual en pesos es del 75%")
     let mount2 = parseInt(prompt("Escribe la cantidad de dinero que vas dejar en el plazo fijo: "))
     let days2 = parseInt(prompt("Escribe la cantidad de dias para calcular el interes: "))
-    let restult2 = (mount2 * 0.75 / 365 * days2);
-    alert("El interes generado por dejar $" + mount2 + " en el plazo fijo durante " + days2 + " es " + restult2);
+    let restult2 = (mount2 * 0.75 / 365 * days2).toFixed(2);
+    alert("El interes generado por dejar $" + mount2 + " en el plazo fijo durante " + days2 + " es $" + restult2);
 }
 
 
